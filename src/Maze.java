d4f88e68302a960e66d67f91ce1a81b8dc255cb1
tawatamasato <donafudo@gmail.com>
2018-12-11 10:03:34 +0900

    Add maze class

---
+++ src/Maze.java
@@ -0,0 +1,16 @@
+public class Maze {
+    char[][] mapdata;
+    int mapSize_X,mapSize_Y;
+    int[] playerStartPos;
+    int[] goalPos;
+
+    // フィールド変数を初期化するぜ
+    public void Maze(String map){
+
+    }
+
+    public char[][] getMapdata(){
+        return mapdata;
+    }
+
+}
