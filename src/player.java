32de13fea8882ce7a9a09e4d9bba3a303fbb48ab
Yusuke OSIRO <e185743@ie.u-ryukyu.ac.jp>
2019-01-15 09:38:10 +0900

    prog2 ex4-2 プレイヤーの簡易設計

---
+++ src/player.java
@@ -0,0 +1,27 @@
+public class player {
+    int[] playerPos;
+    int hohaba = 1;
+    int stepCount;
+    void moveRight(){
+
+    }
+    void moveLeft(){
+
+    }
+    void moveUp(){
+
+    }
+    void moveDown(){
+
+    }
+    public int[] getPlayerPos(){
+        return playerPos;
+    }
+    boolean isWall(int[] posision){
+        return true;
+    }
+    boolean isGoal(int[] goal){
+        return true;
+    }
+}
+
